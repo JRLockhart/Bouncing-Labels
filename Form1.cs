9f83500ebc34de37d141fd9a02d08686a6ca61ef
James Lockhart <james.lockhart117@gmail.com>
2017-06-01 06:30:55 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingLabels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        LabelBouncer[] bouncers = new LabelBouncer[3];
+
+        //
+        private void toggleBouncing(int index, Label labelToBounce)
+        {
+
+            if (bouncers[index] == null)
+            {
+                bouncers[index] = new LabelBouncer();
+                bouncers[index].myLabel = labelToBounce;
+            }
+            else
+            {
+                bouncers[index] = null;
+            }
+
+        }
+
+        //toggle the labels to start moving when the buttons are clicked
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            toggleBouncing(0, label1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            toggleBouncing(1, label2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            toggleBouncing(2, label3);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                if (bouncers[i] != null)
+                {
+                    bouncers[i].Move();
+                }
+            }
+        }
+    }
+}
---
+++ LabelBouncer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingLabels
+{
+    class LabelBouncer
+    {
+
+        //hold a reference to a object that is set to null
+        public Label myLabel;
+
+        //as the label bounces back and forth the value will change from true to false and back again
+        public bool GoingForward = true;
+
+        //method to check if the label has hit the edge of the form
+        public void Move()
+        {
+
+            if (myLabel != null)
+            {
+
+                if (GoingForward == true)
+                {
+
+                    myLabel.Left += 5;
+
+                    if (myLabel.Left >= myLabel.Parent.Width - myLabel.Width)
+                    {
+
+                        GoingForward = false;
+                    }
+                }
+                else
+                {
+                    myLabel.Left -= 5;
+
+                    if (myLabel.Left <= 0)
+                    {
+
+                        GoingForward = true;
+                    }
+                }
+            }
+        }
+    }
+}
